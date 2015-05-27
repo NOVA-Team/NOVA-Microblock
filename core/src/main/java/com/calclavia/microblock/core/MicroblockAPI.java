35cc424c05fcd308cbafb90b2f9ec1e0c5da93b0
Calclavia <admin@calclavia.com>
2015-05-27 17:30:28 +0800

    Added ItemBlockMicro for microblock placement

--- core/src/main/java/com/calclavia/microblock/core/MicroblockAPI.java
+++ core/src/main/java/com/calclavia/microblock/core/MicroblockAPI.java
@@ -1,5 +1,9 @@
 package com.calclavia.microblock.core;
 
+import com.calclavia.microblock.core.micro.BlockMicro;
+import com.calclavia.microblock.core.multi.BlockMulti;
+import nova.core.block.BlockFactory;
+import nova.core.game.Game;
 import nova.core.loader.Loadable;
 import nova.core.loader.NovaMod;
 
@@ -9,4 +13,12 @@ import nova.core.loader.NovaMod;
 @NovaMod(id = "microblock", name = "Microblock", version = "0.0.1", novaVersion = "0.0.1", isPlugin = true)
 public class MicroblockAPI implements Loadable {
 
+	public static BlockFactory microblock;
+	public static BlockFactory multiblock;
+
+	@Override
+	public void preInit() {
+		microblock = Game.instance.blockManager.register(BlockMicro.class);
+		multiblock = Game.instance.blockManager.register(BlockMulti.class);
+	}
 }
