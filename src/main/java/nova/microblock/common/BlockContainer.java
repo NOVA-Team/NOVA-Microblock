fc64fdf846c19f956c22b3307e346603ce1b902a
Kubuxu <kubuxu@gmail.com>
2015-08-12 18:48:21 +0200

    Revert "Revert "Add event passing to Microblocks" as it does not work." as it works
    This reverts commit 1547cf527e10a88cf5a56e4d53f48ac0607c6fd8.

--- src/main/java/nova/microblock/common/BlockContainer.java
+++ src/main/java/nova/microblock/common/BlockContainer.java
@@ -1,5 +1,6 @@
 package nova.microblock.common;
 
+import nova.core.event.Event;
 import nova.microblock.MicroblockPlugin;
 import nova.microblock.micro.MicroblockContainer;
 import nova.microblock.operation.ContainerRemove;
@@ -25,6 +26,9 @@ public class BlockContainer extends Block implements Updater, Stateful, Storable
 	public BlockContainer(String id) {
 		this.id = id;
 		events.on(RemoveEvent.class).bind(evt -> ContainerRemove.interactEventHandler(this, evt));
+		events.on(Event.class).bind(evt ->
+				getOp(MicroblockContainer.class).ifPresent(cont -> cont.microblocks().forEach(uB -> uB.block.events.publish(evt)))
+		);
 	}
 
 	private void printComponents(Collection<Component> components) {
